5c1f31fcf6da78048de05dcf13cc54c3d9858f6e
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-21 15:03:28 +0800

    add web socket

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const serveIndex = require('serve-index');
@@ -24,6 +24,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 app.use('/', serveIndex('public', {'icons': true}));
 app.use('/users', usersRouter);
+
+app.get('/try-sse', (req, res) => {
+  let id = 30;
+
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream; charset=utf-8',
+    'CacheControl': 'no-cache',
+    'Connection': 'keep-alive',
+  });
+
+  setInterval(()=>{
+    const now = new Date();
+    res.write(`id: ${id ++ }\n`);
+    res.write(`data: ${ now.toLocaleString() }\n\n`);
+  }, 2000);
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
